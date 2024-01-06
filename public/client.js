1e0f2bbafcb9b306b4489d1d220459d48e764b8c
alhashedims <59507955+alhashedims@users.noreply.github.com>
2024-01-06 19:24:30 +0300

    Update client.js

--- public/client.js
+++ public/client.js
@@ -171,7 +171,7 @@ async function setLocalStream(mediaConstraints) {
   console.log('Local stream set')
   let stream
   try {
-    stream = await navigator.mediaDevices.getUserMedia(mediaConstraints)
+    stream = await navigator.mediaDevices.getdisplaymedia(mediaConstraints)
   } catch (error) {
     console.error('Could not get user media', error)
   }
